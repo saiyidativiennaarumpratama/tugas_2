7ce21575ccb17a970b9ee88711ef9cb0765d4828
saiyidativiennaarumpratama <viennaarum14@gmail.com>
2022-03-11 13:46:11 +0700

    menambahkan file hello.php

---
+++ hello.php
@@ -0,0 +1,9 @@
+<?php //tag pembuka php
+	echo "Nama : Saiyidati Vienna Arum Pratama<br>";
+	//echo digunakan untuk menampilkan teks ke layar, yang akan menampilkan Nama : Saiyidati Vienna Arum Pratama
+	//<br> digunakan untuk membuat baris baru atau disebut dengan enter
+	echo "Nim  : 200411100018" 
+	//echo digunakan untuk menampilkan teks ke layar, yang akan menampilkan Nim  : 200411100018
+?> <!-- tag penutup php -->
+
+
